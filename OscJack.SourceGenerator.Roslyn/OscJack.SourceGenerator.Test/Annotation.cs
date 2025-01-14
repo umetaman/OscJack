f84fd4d2d3236c5c80d9eab3bd30c563ce13223b
umeta <umeta@w0w.co.jp>
2025-01-14 18:52:23 +0900

    Add SourceGenerator

---
+++ OscJack.SourceGenerator.Roslyn/OscJack.SourceGenerator.Test/Annotation.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace OscJack.Annotation
+{
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Struct, AllowMultiple = false, Inherited = false)]
+    public class OscPackableAttribute : Attribute { }
+
+    [AttributeUsage(AttributeTargets.Property | AttributeTargets.Field, AllowMultiple = false, Inherited = false)]
+    public class OscElementOrderAttribute : Attribute
+    {
+        public int Index { get; private set; }
+
+        public OscElementOrderAttribute(int index)
+        {
+            this.Index = index;
+        }
+    }
+}
